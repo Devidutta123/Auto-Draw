01648d62f966c011ff719eff6ba042e130305510
Devidutta <90234585+Devidutta123@users.noreply.github.com>
2022-04-04 09:16:02 +0530

    Update python-auto-draw.py

--- python-auto-draw.py
+++ python-auto-draw.py
@@ -8,16 +8,18 @@ time.sleep(5)
 pyautogui.click()
 
 # can be varied according to convininence
-distance = 500
+distance = 400
 
 while distance > 0:
     # right
     pyautogui.dragRel(distance, 0, duration = 0.1)
     
-    distance -= 10
+    distance -= 5
 
     # down
     pyautogui.dragRel(0, distance, duration = 0.1)
+    
+    distance -= 5
 
     # left
     pyautogui.dragRel(-distance, 0, duration = 0.1)
@@ -26,4 +28,6 @@ while distance > 0:
 
     #up
     pyautogui.dragRel(0, -distance, duration = 0.1)
+    
+    distance -= 5
 
